c859d9b026fc248305788b67e6855038939a5476
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-03-31 13:14:33 -0500

    Add @ngxs/store and ngx-toastr dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    container:{
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1140px",
+        "2xl": "1320px",
+      }
+    }
   },
   plugins: [],
 }
