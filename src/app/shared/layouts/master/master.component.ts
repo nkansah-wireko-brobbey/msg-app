5038907b8c5adbbdc86e68a4841fa6a60244f29e
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-03-31 13:15:23 -0500

    Add header component and router outlet to master layout

--- src/app/shared/layouts/master/master.component.ts
+++ src/app/shared/layouts/master/master.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-master',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, CommonModule, HeaderComponent],
   templateUrl: './master.component.html',
   styleUrl: './master.component.scss'
 })
 export class MasterComponent {
-
+  constructor() {}
 }
