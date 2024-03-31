7dfbb4b892dac0579c72714a8b83bca9be1ed82e
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-03-31 13:16:09 -0500

    Add login form component and card component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CardComponent } from '../../shared/components/card/card.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+
+  constructor(private toastService: ToastrService) {}
+  openToast() {
+    this.toastService.success('Hello world!', 'Toastr fun!');
+  }
 }
