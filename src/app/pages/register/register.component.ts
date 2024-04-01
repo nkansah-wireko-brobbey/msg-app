f30f16fd5693ea510f9e569a103d3cc1a3802edf
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-03-31 19:27:48 -0500

    Add registration form to register component

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CardComponent } from '../../shared/components/card/card.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
