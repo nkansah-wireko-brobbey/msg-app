344908b4599f315b948f89f4f692fee8e37c87a1
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-03-31 19:28:36 -0500

    Refactor messages component layout and imports

--- src/app/pages/messages/messages.component.ts
+++ src/app/pages/messages/messages.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CardComponent } from '../../shared/components/card/card.component';
+import { SidebarComponent } from '../../shared/layouts/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [CardComponent,SidebarComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.scss'
 })
