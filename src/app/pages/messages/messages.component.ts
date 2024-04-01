765889fc4f7c33c718558f0bcfde3eb7cb73ad4b
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-03-31 20:56:09 -0500

    Add  message item to messages component

--- src/app/pages/messages/messages.component.ts
+++ src/app/pages/messages/messages.component.ts
@@ -1,11 +1,12 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CardComponent } from '../../shared/components/card/card.component';
 import { SidebarComponent } from '../../shared/layouts/sidebar/sidebar.component';
+import { MessageItemComponent } from '../../shared/components/message-item/message-item.component';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [CardComponent,SidebarComponent],
+  imports: [CardComponent,SidebarComponent, MessageItemComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.scss'
