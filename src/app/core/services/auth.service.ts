80280bd94b62792e079e909ec8f4940d0dab4183
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-15 20:10:40 -0500

    Update login method in AuthService to use HttpClient for API call

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,17 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Ilogin } from '../models/auth.model';
+import { apiEndpoint } from '../constants/constatns';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  login(){
-
+  login(loginData: Ilogin){
+      this.http.post(`${apiEndpoint.authEndpoint.login}`, loginData)
   }
 
   logout(){
-    
+
   }
 }
