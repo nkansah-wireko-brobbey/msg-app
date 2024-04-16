3e2e68881fd62c242c0a4c3f2deee05c84e7db5a
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-15 20:11:40 -0500

    Setup reactive forms for login form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,20 +1,43 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { CardComponent } from '../../shared/components/card/card.component';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, ReactiveFormsModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup;
 
 
-  constructor(private toastService: ToastrService) {}
+  constructor(
+    private toastService: ToastrService,
+    private fb: FormBuilder
+  ) {
+
+  }
   openToast() {
     this.toastService.success('Hello world!', 'Toastr fun!');
+  }
+
+  ngOnInit(): void {
+      this.loginForm = this.fb.group({
+        email: new FormControl('',[Validators.required, Validators.email]),
+        password: new FormControl('',[Validators.required])
+      })
+  }
+
+  onSubmit(){
+      if(this.loginForm.valid){
+          console.log(this.loginForm.value)
+      }else{
+        this.loginForm.markAllAsTouched();
+      }
   }
 }
