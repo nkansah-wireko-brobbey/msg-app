360279c18a42af8e503167ac015fdc3a5f1b1042
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-16 21:09:10 -0500

    Add login functionality to LoginComponent

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { CardComponent } from '../../shared/components/card/card.component';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -18,7 +19,8 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private toastService: ToastrService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private authService: AuthService
   ) {
 
   }
@@ -36,6 +38,18 @@ export class LoginComponent implements OnInit {
   onSubmit(){
       if(this.loginForm.valid){
           console.log(this.loginForm.value)
+          this.authService.login(this.loginForm.value)
+          .subscribe({
+            next(res){
+              console.log(res)
+            },
+            error(error){
+              console.log(error)
+            }
+
+            
+          })
+          
       }else{
         this.loginForm.markAllAsTouched();
       }
