007aee0d18bd3b152b39b36698fac00816e284ad
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-16 21:08:34 -0500

    Update TokenService to include authentication status and token management

--- src/app/core/services/token.service.ts
+++ src/app/core/services/token.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import {constants} from '../constants/constants'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
 
-  constructor() { }
+  isAuthenticated: BehaviorSubject<boolean> = new BehaviorSubject(false)
+
+  constructor() { 
+
+    const token = this.getToken()
+    if(token) this.updateToken(true);
+
+  }
+
+  updateToken(status: boolean){
+    this.isAuthenticated.next(status);
+  }
+
+  getToken() : string | null{
+    return localStorage.getItem(constants.CURRENT_TOKEN) || null;
+  }
+
+  setToken(token: string){
+    localStorage.setItem(constants.CURRENT_TOKEN,'token');
+    this.updateToken(true)
+  }
+
+  removeToken(){
+    localStorage.removeItem(constants.CURRENT_TOKEN)
+    this.updateToken(false)
+  }
 }
