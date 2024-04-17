43f5c248bc87a12ffbe2e38c835267c9d8aa9963
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-16 21:07:36 -0500

    Add HttpClientModule to app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { routes } from './app.routes';
@@ -10,10 +11,12 @@ import { provideToastr } from 'ngx-toastr';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
-    importProvidersFrom(NgxsModule.forRoot()), provideAnimations(),
+    importProvidersFrom(NgxsModule.forRoot(), HttpClientModule), provideAnimations(),
     provideToastr({
       timeOut: 10000,
       positionClass: 'toast-top-right',
       preventDuplicates: true,
-    })]
+    }),
+     
+  ]
 };
