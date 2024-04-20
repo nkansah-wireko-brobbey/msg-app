64ca8b32c43514cff60a5406afa3b0759cd2df7c
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-20 00:05:38 -0500

    Update message-item component to use data bindings for sender name, subject, and createdAt

--- src/app/shared/components/message-item/message-item.component.ts
+++ src/app/shared/components/message-item/message-item.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IMessage } from '../../../core/models/common.model';
+import { DateAgoPipe } from '../../pipes/date-ago.pipe';
+import { NameInitialsPipe } from '../../pipes/name-initials.pipe';
 
 @Component({
   selector: 'app-message-item',
   standalone: true,
-  imports: [],
+  imports: [DateAgoPipe, NameInitialsPipe],
   templateUrl: './message-item.component.html',
   styleUrl: './message-item.component.scss'
 })
 export class MessageItemComponent {
+
+  @Input() data!: IMessage;
+
+  constructor(){
+    console.log(this.data)
+  }
 
 }
