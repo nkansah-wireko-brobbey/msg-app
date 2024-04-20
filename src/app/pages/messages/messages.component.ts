9bb5a8c0e1678306ae6942a1fdc1895ea2ecb2df
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-19 21:25:43 -0500

    Refactor MessagesComponent to use Ngxs store for managing message state

--- src/app/pages/messages/messages.component.ts
+++ src/app/pages/messages/messages.component.ts
@@ -3,6 +3,10 @@ import { CardComponent } from '../../shared/components/card/card.component';
 import { SidebarComponent } from '../../shared/layouts/sidebar/sidebar.component';
 import { MessageItemComponent } from '../../shared/components/message-item/message-item.component';
 import { MessageService } from '../../core/services/message.service';
+import { Select, Store } from '@ngxs/store';
+import { GetAllMessages, MessageState } from '../../store/MessageState';
+import { Observable } from 'rxjs';
+import { IMessage } from '../../core/models/common.model';
 
 @Component({
   selector: 'app-messages',
@@ -14,27 +18,28 @@ import { MessageService } from '../../core/services/message.service';
 })
 export class MessagesComponent implements OnInit{
 
+  @Select(MessageState.selectMessages) message$!: Observable<IMessage[]>;
+
  constructor(
-  private messageService: MessageService
+  private messageService: MessageService,
+  private store: Store
  ){
 
  }
 
  ngOnInit(): void {
-     this.getAllMessages()
- }
-
- getAllMessages(){
-    this.messageService.getAllMessages()
-    .subscribe({
-      next(res){
-        console.log(res)
-      },
-      error(error){
-        console.log(error)
+    //  this.getAllMessages()
+    this.message$.subscribe({
+      next:(value)=>{
+        if(!value.length){
+            this.store.dispatch(new GetAllMessages())
+            console.log(value)
+        }
       }
     })
-  }
+ }
+
+
 
 
 }
