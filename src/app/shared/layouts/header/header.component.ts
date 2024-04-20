16350b8291f883d62a508d19573ca484f7b83670
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-20 00:10:16 -0500

    Update header component to conditionally display login and register links based on user authentication status

--- src/app/shared/layouts/header/header.component.ts
+++ src/app/shared/layouts/header/header.component.ts
@@ -2,18 +2,26 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TokenService } from '../../../core/services/token.service';
 import { AuthService } from '../../../core/services/auth.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  isAuthenticated$!: Observable<boolean>;
+
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private tokenService: TokenService
   ){
+
+    this.isAuthenticated$ = tokenService.isAuthenticated
 
   }
 logout() {
