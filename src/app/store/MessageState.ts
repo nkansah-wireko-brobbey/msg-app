8defed1b1d77c3a3fcd8da314a6314187936947f
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-19 21:24:58 -0500

    Add MessageState to manage message-related state in the application

---
+++ src/app/store/MessageState.ts
@@ -0,0 +1,53 @@
+import { Action, Selector, State, StateContext } from "@ngxs/store";
+import { IMessage } from "../core/models/common.model";
+import { Injectable } from "@angular/core";
+import { MessageService } from "../core/services/message.service";
+import { tap } from "rxjs";
+
+export class GetAllMessages{
+    static readonly type = '[Message] Get All';
+}
+
+export interface MessageStateModel{
+    messages: IMessage[] | undefined
+}
+
+@State<MessageStateModel>({
+    name: 'Message',
+    defaults:{
+        messages:[]
+    }
+})
+
+@Injectable()
+export class MessageState{
+
+    constructor(private messageService: MessageService){
+
+    }
+
+    @Action(GetAllMessages)
+    getAllMessage(ctx: StateContext<MessageStateModel>){
+
+        return this.messageService.getAllMessages()
+        .pipe(
+            tap((res)=>{
+                const state = ctx.getState();
+                ctx.setState({
+                    ...state,
+                    messages: res.data
+                })
+
+            })
+        )
+
+    }
+
+    @Selector([MessageState])
+    static selectMessages(state: MessageStateModel):IMessage[] | undefined{
+        return state.messages;
+    }
+
+
+
+}
