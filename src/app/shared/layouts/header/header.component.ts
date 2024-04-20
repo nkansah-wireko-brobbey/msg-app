d79f0eac8870477002b5c131eae5cedc084a29e8
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-19 21:25:11 -0500

    Add authentication and logout functionality to header component

--- src/app/shared/layouts/header/header.component.ts
+++ src/app/shared/layouts/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TokenService } from '../../../core/services/token.service';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,5 +11,17 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  constructor(
+    private authService: AuthService
+  ){
+
+  }
+logout() {
+  this.authService.logout().subscribe({
+    next(){
+      
+    }
+  })
+}
 
 }
