eda5b999500c9fc63c571dcb2b70f5559d47dbb7
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-24 11:21:04 -0500

    Update app.component.ts to include SocketService and UserState in imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SocketService } from './core/services/socket.service';
+import { Select, Store } from '@ngxs/store';
+import { GetLoggedInUser, UserState } from './store/UserState';
+import { Observable } from 'rxjs';
+import { IUser } from './core/models/auth.model';
+import { TokenService } from './core/services/token.service';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +17,37 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'msg-app';
+
+  @Select(UserState.selectUser) user$!: Observable<IUser>;
+
+  constructor(
+    private socketService: SocketService,
+    private store: Store,
+    private tokenService: TokenService
+  ) {
+
+    tokenService.isAuthenticated.subscribe({
+      next: (value) => {
+        if(value){
+          this.store.dispatch(new GetLoggedInUser())
+        }
+      }
+    });
+    
+    this.user$.subscribe({
+      next: (value) => {
+        if(value){
+          this.socketService.saveUserIdWithSocket(value._id)
+        }
+      }
+    });
+
+    this.socketService.socketInit().subscribe(
+      {
+        next: (res) => {
+          console.log("Socket Id: ", res);
+        }
+      }
+    );
+  }
 }
