07b2d2898246023c9645fd650d8675e156389c77
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-25 00:07:33 -0500

    Add FilterService to handle filtering of messages in MessagesComponent

---
+++ src/app/core/services/filter.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { IMessage } from '../models/common.model';
+import { Select, Store } from '@ngxs/store';
+import { UserState } from '../../store/UserState';
+import { IUser } from '../models/auth.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilterService {
+
+  @Select(UserState.selectUser) user$!: Observable<IUser>;
+
+  userId!: string;
+
+  constructor(
+    private store: Store
+  ) { 
+    this.user$.subscribe({
+      next: (value) => {
+        if(value)
+        this.userId = value._id
+          console.log("User Id: ", value);
+      }
+    });
+  }
+
+  public filterData(data: Observable<IMessage[]>, filter: string) {
+
+
+
+    if (!this.filterCriteria(filter)) {
+      return data;
+    }
+    return this.filterCriteria(filter) === "trash" ? this.filterInTrash(data) : this.filterInSent(data);
+  }
+
+  private filterCriteria(filter: string) : false | "trash" | "sent"{
+
+    return filter === "trash" ? "trash" : filter === "sent" ? "sent" : false;
+
+  }
+
+  private filterInTrash(data: Observable<IMessage[]>) {
+    return data.pipe(
+      map((res) => {
+        return res.filter((item) => {
+          return item.status == 3;
+        });
+      }
+    ))
+  }
+
+  private filterInSent(data: Observable<IMessage[]>) {
+    return data.pipe(
+      map((res) => {
+        return res.filter((item) => {
+          return item.sender._id === this.userId;
+        });
+      }
+    ))
+  }
+}
