2f20be647adb0db141cfbbd347f55026d26fcfae
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-25 00:08:27 -0500

    Refactor MessagesComponent to use messageData$ and add URL and filter services

--- src/app/pages/messages/messages.component.ts
+++ src/app/pages/messages/messages.component.ts
@@ -4,9 +4,13 @@ import { SidebarComponent } from '../../shared/layouts/sidebar/sidebar.component
 import { MessageItemComponent } from '../../shared/components/message-item/message-item.component';
 import { Select, Store } from '@ngxs/store';
 import { GetAllMessages, GetNewMessage, MessageState } from '../../store/MessageState';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable, of, switchMap } from 'rxjs';
 import { IMessage } from '../../core/models/common.model';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
+import { FilterService } from '../../core/services/filter.service';
+import { ActivatedRoute } from '@angular/router';
+import { map, startWith } from 'rxjs/operators';
+import { UrlService } from '../../core/services/url.service';
 
 @Component({
   selector: 'app-messages',
@@ -20,28 +24,48 @@ export class MessagesComponent implements OnInit, OnDestroy{
 
   @Select(MessageState.selectMessages) message$!: Observable<IMessage[]>;
 
+  messageData$!: Observable<IMessage[]>;
+
  constructor(
-  private store: Store
+  private store: Store,
+  private filterService: FilterService,
+  private urlService: UrlService
+
  ){
 
  }
 
  ngOnInit(): void {
-    //  this.getAllMessages()
-
 
     this.store.dispatch([new GetAllMessages(), new GetNewMessage()])
     console.log("Message Here")
     this.message$.subscribe({
       next:(value)=>{
         if(value.length){
-            console.log(value)
+          this.messageData$ = this.message$
         }
         console.log('in here',value)
       }
     })
-    
+
+    this.urlService.getURL().pipe(
+      switchMap(filterType => {
+        // Filter messages based on the URL segment
+        return this.filterService.filterData(this.message$, filterType);
+      })
+    ).subscribe({
+      next: filteredMessages => {
+        // Update the messageData$ observable with filtered messages
+        this.messageData$ = of(filteredMessages);
+      },
+      error: err => {
+        // Handle errors
+        console.error('Error filtering messages:', err);
+      }
+    });
  }
+
+
  ngOnDestroy(): void {
 
   console.log("Destroy Run")
