bdb992d1b440ccf05f89e8ea971552153d1c3093
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-28 22:39:05 -0500

    Refactor modal component to use Observables in ModalComponent

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { IMessage } from '../../../core/models/common.model';
 import { ModalService } from '../../../core/services/modal.service';
+import { Observable } from 'rxjs';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -9,9 +11,45 @@ import { ModalService } from '../../../core/services/modal.service';
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
-export class ModalComponent {
-  @Input() data!: IMessage;
+export class ModalComponent implements OnInit{
+  modalObservable!: Observable<IMessage>;
+  showModalObservable!: Observable<boolean>;
+
+  modalData!: IMessage;
+
+  showModal = false;
+
+
+
   constructor(
     private modalService: ModalService
   ) {}
+
+  ngOnInit() {
+
+    this.modalObservable = this.modalService.getModalData();
+
+    this.modalObservable.subscribe({
+      next: (value) => {
+        this.modalData = value;
+        console.log('modalData', value)
+      }
+    })
+
+    this.showModalObservable = this.modalService.getShowModal();
+
+    this.showModalObservable.subscribe({
+      next: (value) => {
+        this.showModal = value;
+        console.log('showModal', value)
+      }
+    })
+   
+  }
+
+  closeModal(){
+    this.modalService.hideModal();
+    this.modalService.clearModalData();
+  }
+
 }
