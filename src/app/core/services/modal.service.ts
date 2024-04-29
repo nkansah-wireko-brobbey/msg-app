5963918a73865f4ebd01176572bfee63857e3122
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-28 22:36:14 -0500

    Update ModalService to use BehaviorSubject for modalData and add displayModal and hideModal methods

--- src/app/core/services/modal.service.ts
+++ src/app/core/services/modal.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { IMessage } from '../models/common.model';
 
 @Injectable({
@@ -9,7 +9,7 @@ export class ModalService {
 
   private showModal: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-  private modalData: Observable<IMessage> = new Observable();
+  private modalData: BehaviorSubject<IMessage> = new BehaviorSubject<IMessage>({} as IMessage);
 
   constructor() { }
 
@@ -18,21 +18,27 @@ export class ModalService {
   }
 
   public setModalData(data: IMessage): void {
-    this.modalData = new Observable(subscriber => {
-      subscriber.next(data);
-    });
+    this.modalData.next(data);
   }
 
   public getShowModal(): Observable<boolean> {
     return this.showModal;
   }
 
-  public setShowModal(status: boolean): void {
+  private setShowModal(status: boolean): void {
     this.showModal.next(status);
   }
 
+  public displayModal(): void {
+    this.setShowModal(true);
+  }
+
+  public hideModal(): void {
+    this.setShowModal(false);
+  }
+
   public clearModalData(): void {
-    this.modalData = new Observable();
+    this.modalData.next({} as IMessage);
   }
 
 
