c969b48c2a09e856a6d2682d027678a7d0af489e
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-08 00:28:00 -0500

    Switched to prod

--- src/app/core/constants/constants.ts
+++ src/app/core/constants/constants.ts
@@ -2,8 +2,8 @@ export const constants = {
     CURRENT_TOKEN: 'CURRENT_TOKEN',
 }
 
-// const APIURL = 'https://msg-box-backend-pzrzz1prd-nkansah-wireko-brobbeys-projects.vercel.app'
-const APIURL = 'http://localhost:4000'
+const APIURL = 'https://msg-box-backend-pzrzz1prd-nkansah-wireko-brobbeys-projects.vercel.app'
+// const APIURL = 'http://localhost:4000'
 
 export const apiEndpoint = {
     authEndpoint: {
